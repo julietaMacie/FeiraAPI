1ad66ef97ac87b0916a40e9c320c4e7c02435a1e
julietaMacie <julietacarlosmacie@gmail.com>
2025-06-07 04:55:30 +0200

    docs: adiciona configuração do Swagger/OpenAPI para documentação

--- src/main/java/com/MacieNhangumele/FeiraAPI/configuration/SwaggerConfig.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/configuration/SwaggerConfig.java
@@ -1,6 +1,19 @@
 package com.MacieNhangumele.FeiraAPI.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
-
+    @Bean
+    public OpenAPI feiraOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Feira API")
+                .description("API para gestão de feiras online e presenciais")
+                .version("1.0")
+                .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
 }
