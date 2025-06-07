06253141e31887b87cba2d9c7da569d29ba69345
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:34:00 +0200

    feat: cria repositório Visitante com buscas por nome, tipoAcesso e usuário

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/VisitanteRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/VisitanteRepository.java
@@ -1,5 +1,11 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
-public interface VisitanteRepository  {
+import com.MacieNhangumele.FeiraAPI.models.Visitante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+public interface VisitanteRepository extends JpaRepository<Visitante, Long> {
+    List<Visitante> findByNomeContainingIgnoreCase(String nome);
+    List<Visitante> findByTipoAcesso(String tipoAcesso);
+    Visitante findByUserId(Long userId);
 }
