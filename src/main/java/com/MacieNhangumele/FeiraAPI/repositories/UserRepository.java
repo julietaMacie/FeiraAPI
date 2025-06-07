7416d4ac89fffff39af7e83677d870807da096e8
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:33:53 +0200

    feat: adiciona repositório User com métodos para autenticação (email)

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/UserRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/UserRepository.java
@@ -1,6 +1,13 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
+import com.MacieNhangumele.FeiraAPI.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository  {
+import java.util.Optional;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
