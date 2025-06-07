8ad3a0edcbe19bf60b15c40789ee08e6839a9a0f
julietaMacie <julietacarlosmacie@gmail.com>
2025-06-07 04:04:21 +0200

    chore: configuração inicial do projecto

---
+++ src/main/java/com/MacieNhangumele/FeiraAPI/FeiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.MacieNhangumele.FeiraAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MacieNhangumele/FeiraAPI/FeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MacieNhangumele.FeiraAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
