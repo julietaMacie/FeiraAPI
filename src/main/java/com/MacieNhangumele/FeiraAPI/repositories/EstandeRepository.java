1c840a2f306824c1e1ffc07efa4c48dfb32df0c2
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:32:23 +0200

    feat: implementa repositório para Estande com métodos de busca personalizados

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/EstandeRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/EstandeRepository.java
@@ -1,6 +1,11 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
+import com.MacieNhangumele.FeiraAPI.models.Estande;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface EstandeRepository {
-
+public interface EstandeRepository extends JpaRepository<Estande, Long> {
+    List<Estande> findByLocalizacaoContainingIgnoreCase(String localizacao);
+    List<Estande> findByStatus(String status);
+    List<Estande> findByExpositorId(Long expositorId);
 }
