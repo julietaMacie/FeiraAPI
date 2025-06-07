5eb852dd9d7b58faf71460446404420b6c7e3cf9
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:33:25 +0200

    feat: adiciona repositório para Evento com filtros por título, data e expositor

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/EventoRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/EventoRepository.java
@@ -1,5 +1,12 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
-public interface EventoRepository {
+import com.MacieNhangumele.FeiraAPI.models.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+    List<Evento> findByTituloContainingIgnoreCase(String titulo);
+    List<Evento> findByDataBetween(LocalDateTime start, LocalDateTime end);
+    List<Evento> findByExpositorId(Long expositorId);
 }
