294cb2a96872c6fb41c74b8d1f40da4f339ba756
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:33:35 +0200

    feat: cria repositório para Expositor com consultas por nome, tipo e usuário

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/ExpositorRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/ExpositorRepository.java
@@ -1,6 +1,11 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
+import com.MacieNhangumele.FeiraAPI.models.Expositor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface ExpositorRepository{
-
+public interface ExpositorRepository extends JpaRepository<Expositor, Long> {
+    List<Expositor> findByNomeContainingIgnoreCase(String nome);
+    List<Expositor> findByTipo(String tipo);
+    Expositor findByUserId(Long userId);
 }
