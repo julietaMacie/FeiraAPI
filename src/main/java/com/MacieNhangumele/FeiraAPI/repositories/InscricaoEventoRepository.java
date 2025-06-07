843429cfe59654364ea85dd695d2a2ad37762a3d
DevsibusisoNhangumele <sibusisonhangumelee42@gmail.com>
2025-06-07 05:33:46 +0200

    feat: implementa repositório para InscricaoEvento com métodos de relacionamento

--- src/main/java/com/MacieNhangumele/FeiraAPI/repositories/InscricaoEventoRepository.java
+++ src/main/java/com/MacieNhangumele/FeiraAPI/repositories/InscricaoEventoRepository.java
@@ -1,6 +1,11 @@
 package com.MacieNhangumele.FeiraAPI.repositories;
 
+import com.MacieNhangumele.FeiraAPI.models.InscricaoEvento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface InscricaoEventoRepository  {
-
+public interface InscricaoEventoRepository extends JpaRepository<InscricaoEvento, Long> {
+    List<InscricaoEvento> findByEventoId(Long eventoId);
+    List<InscricaoEvento> findByVisitanteId(Long visitanteId);
+    boolean existsByEventoIdAndVisitanteId(Long eventoId, Long visitanteId);
 }
